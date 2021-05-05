760b8b150061b32e165f68bff8ce1fadf6212bb1
rubic <rmdrivs@gmail.com>
2021-05-05 19:20:03 +0200

    Clase main 1.0

---
+++ src/proyecto_bases/Ejecuta_consultas.java
@@ -0,0 +1,13 @@
+package proyecto_bases;
+
+public class Ejecuta_consultas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			Consulta Consulta_1=new Consulta();
+			
+			Consulta_1.Recorre_consulta();
+	}
+
+}
